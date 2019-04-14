1bd8aba88db9f34a6ee47702a5e12b8b9eb635b8
Alexandros Baramilis <alexbaramilis@hotmail.com>
2019-04-14 17:48:30 +0100

    Picking up where Part 1 left off

---
+++ Breather Part 2/Supporting Files/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  Breather Part 2
+//
+//  Created by Alexandros Baramilis on 14/04/2019.
+//  Copyright © 2019 Alexandros Baramilis. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}
---
+++ Breather Part 2/Supporting Files/Constants/Text.swift
@@ -0,0 +1,27 @@
+//
+//  Text.swift
+//  Breather Part 2
+//
+//  Created by Alexandros Baramilis on 14/04/2019.
+//  Copyright © 2019 Alexandros Baramilis. All rights reserved.
+//
+
+import Foundation
+
+enum Text {
+    /// Returns a string describing the conditions of the range the given AQI is in.
+    ///
+    /// The ranges and labels are the same for AQI US and AQI China.
+    static func forAQI(_ aqi: Int) -> String {
+        switch aqi {
+        case 0...50: return "Good"
+        case 51...100: return "Moderate"
+        case 101...150: return "Unhealthy for Sensitive Groups"
+        case 151...200: return "Unhealthy"
+        case 201...300: return "Very Unhealthy"
+        case 301...500: return "Hazardous"
+        case 501...Int.max: return "F**k"
+        default: return "Invalid Data"
+        }
+    }
+}
